d7f60606ef54a7abf35047150b17c039d209abac
Noah Wang <1057398161@qq.com>
2021-11-24 16:58:13 +0800

    :new: openprompt

---
+++ framework/methodology/openprompt/__init__.py
@@ -0,0 +1,9 @@
+import openprompt
+
+'''
+learning material : please see the readme file
+api : also in the readme file
+my learning code : application in fewqa repo. 
+'''
+
+pass
---
+++ framework/methodology/openprompt/config.py
@@ -0,0 +1,2 @@
+
+path_to_model = '/nfs/users/wangzekun/repos/fewqa/assets/models/mengzi-bert-base-fin'
